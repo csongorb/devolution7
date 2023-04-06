82fb4d4abae831d9915f21c249f942d5bc00ee41
Maria Shchurova <mary.shchurova@gmail.com>
2023-04-06 23:43:28 +0200

    more items

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -4,6 +4,11 @@ import "./Card.css"
 
 //todo deal with images better
 import image1 from "../../src/assets/images/lmd_1.jpg"
+import image2 from "../../src/assets/images/lmd_2.jpg"
+import image3 from "../../src/assets/images/lmd_3.jpg"
+import image4 from "../../src/assets/images/lmd_4.jpg"
+import image5 from "../../src/assets/images/lmd_5.jpg"
+import image6 from "../../src/assets/images/lmd_6.jpg"
 
 function Cards() {
   return (
@@ -15,6 +20,42 @@ function Cards() {
                     <CardItem 
                     src={image1}
                     text="Version 1"
+                    label="Version"
+                    path="/services"
+                    />
+                    <CardItem 
+                    src={image2}
+                    text="Version 2"
+                    label="Version"
+                    path="/services"
+                    />
+                </ul> 
+                
+                <ul className='cards__items'>
+                    <CardItem 
+                    src={image3}
+                    text="Version 3"
+                    label="Version"
+                    path="/services"
+                    />
+                    <CardItem 
+                    src={image4}
+                    text="Version 4"
+                    label="Version"
+                    path="/services"
+                    />
+                </ul>
+                <ul className='cards__items'>
+                    <CardItem 
+                    src={image5}
+                    text="Version 5"
+                    label="Version"
+                    path="/services"
+                    />
+               
+                    <CardItem 
+                    src={image6}
+                    text="Version 6"
                     label="Version"
                     path="/services"
                     />
