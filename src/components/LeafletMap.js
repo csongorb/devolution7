0b873d0ac86306f150698b0e1643de3a9ec96082
Maria Shchurova <mary.shchurova@gmail.com>
2023-04-13 02:56:41 +0200

    managed to put it in center

--- src/components/LeafletMap.js
+++ src/components/LeafletMap.js
@@ -1,28 +1,34 @@
 import React from 'react'
-import { MapContainer, SVGOverlay } from 'react-leaflet'
+import { MapContainer, SVGOverlay, Marker, Popup } from 'react-leaflet'
 import "./LeafletMap.css"
 
 import {ReactComponent as SvgElement0} from './bg.svg';
 import {ReactComponent as SvgElement1} from './tree.svg';
+//import {ReactComponent as SvgElement2} from './diagram50.svg';
+
 function LeafletMap ()
 {
-    const position = [51.505, -0.09]
-    const bounds = [[51.49, -0.08],  [51.5, -0.06],]
+    const position = [22, -115]
+    const bounds = [[32, -130],  [13, -100]]
 
     
     return(
-        <div id="map">            
-            <MapContainer  center={position} zoom={15} scrollWheelZoom={false}
-            style={{height: "100vh"}}
+        <div className="map-container">            
+            <MapContainer  center={position} zoom={6} scrollWheelZoom={false}
+            style={{height: 700, width : "100%"}}
             >
-            <SVGOverlay bounds={bounds} zIndex={0}>
+            <SVGOverlay  bounds={bounds} zIndex={0}>
                 <SvgElement0 />
             </SVGOverlay>
-
-            <SVGOverlay bounds={bounds} zIndex={1}>
+            <SVGOverlay  bounds={bounds} zIndex={1} >
                 <SvgElement1 />
             </SVGOverlay>
 
+            <Marker position={bounds[0]}>
+                <Popup>
+                    A pretty CSS3 popup. <br /> Easily customizable.
+                </Popup>
+            </Marker>
             </MapContainer>
         </div>
     )
