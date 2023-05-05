7cfb88b6a021bb1dd61745487bd8115976e072c0
Maria Shchurova <mary.shchurova@gmail.com>
2023-05-05 08:35:43 +0200

    more or less right structure for bounds (based on MapTiler output)
    needs new output file from Christoph

--- src/components/LeafletMap.js
+++ src/components/LeafletMap.js
@@ -1,73 +1,49 @@
-import React from 'react'
-import { MapContainer, TileLayer, Marker, Popup, Rectangle, ImageOverlay, SVGOverlay } from 'react-leaflet'
-//import {ReactComponent as SvgElement1} from './tree.svg';
+import { MapContainer, TileLayer } from 'react-leaflet';
 import L from 'leaflet';
 
-function LeafletMap ()
-{
-  const tileSize = 126;  // size of your custom tiles
-  const minCoord = L.point(0, 0);  // minimum coordinate of your tile grid
-  const maxCoord = L.point(129, 154);  // maximum coordinate of your tile grid
-  const minLatLng = L.latLng(
-    maxCoord.y * tileSize,  // convert y coordinate to latitude
-    minCoord.x * tileSize   // convert x coordinate to longitude
+
+function LeafletMap() {
+  const mapExtent = [0.00000000, -39288.00000000, 33000.00000000, 0.00000000];
+  const mapMinZoom = 2;
+  const mapMaxZoom = 8;
+  const mapMaxResolution = 0.50000000;
+  const mapMinResolution = Math.pow(2, mapMaxZoom) * mapMaxResolution;
+  const tileExtent = [0.00000000, -39288.00000000, 33000.00000000, 0.00000000];
+
+  const crs = L.CRS.Simple;
+  crs.transformation = new L.Transformation(1, -tileExtent[0], -1, tileExtent[3]);
+  crs.scale = function (zoom) {
+    return Math.pow(2, zoom) / mapMinResolution;
+  };
+  crs.zoom = function (scale) {
+    return Math.log(scale * mapMinResolution) / Math.LN2;
+  };
+
+
+  return (
+    <div>
+      <MapContainer
+      center={[0, 0]} 
+      zoom={2}  
+      minZoom={mapMinZoom}
+      maxZoom={mapMaxZoom}
+      bounds={[
+        crs.unproject(L.point(mapExtent[2], mapExtent[3])),
+        crs.unproject(L.point(mapExtent[0], mapExtent[1]))
+      ]}
+      style={{height: 700, width : "100%"}}>
+
+      <TileLayer
+      minZoom={mapMinZoom}
+      maxZoom={mapMaxZoom}
+      tileSize={L.point(512, 512)}
+      noWrap={true}
+      tms={false}
+      url="https://raw.githubusercontent.com/csongorb/devolution7/master/src/components/CustomTiles/{z}/{x}/{y}.png"/>
+
+    </MapContainer>
+    </div>
   );
-  const maxLatLng = L.latLng(
-    minCoord.y * tileSize + tileSize,  // convert y coordinate to latitude
-    maxCoord.x * tileSize + tileSize   // convert x coordinate to longitude
-  );
-  //const bounds = L.latLngBounds(minLatLng, maxLatLng);
-  const bounds = [
-    [51.49, -0.08], //sw
-    [19404, 16380], //ne
-  ]
-
-    function handleClick(e) {
-      if (e.pointerType === 'touch') {
-        console.log(`Clicked with touch at coordinates: ${e.latlng}`);
-        console.log(`Pressure applied: ${e.pressure}`);
-      } else {
-        console.log(`Clicked with mouse at coordinates: ${e.latlng}`);
-      }
-    }
-
-    
-    return(
-        <div >
-            <MapContainer  center={[0, 0]} zoom={0} minZoom={0} maxZoom={8}  onClick={handleClick} style={{height: 700, width : "100%"}}>
-              <TileLayer
-                url="https://raw.githubusercontent.com/csongorb/devolution7/master/src/components/CustomTiles/{z}/{x}/{y}.png"
-                noWrap={true}
-                name="Test"
-                bounds={bounds}
-                maxZoom={8}
-                />
-
-{ /*         <Marker position={center}>
-                <Popup>
-                Link<br />
-                <a target="_blank" rel="noreferrer" href="https://devolution.online/">here</a>
-                </Popup>
-              </Marker>
-
-              <ImageOverlay
-              url="https://de.wikipedia.org/wiki/House#/media/Datei:Paradise_garage.jpg"
-              bounds={[center, [-35.8650, 154.2094]]}
-              />
-
-              <Rectangle
-                bounds={[center,[54, 6],]} pathOptions={{ color: 'cyan' }}>
-                <Popup>
-                <iframe width="300" height="169" src="https://www.youtube.com/embed/6O6V7L2IByg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-                </Popup>
-              </Rectangle>
-              <SVGOverlay  bounds={bounds} zIndex={1000}>
-                <SvgElement1 />
-            </SVGOverlay>
-*/}
-            </MapContainer>
-        </div>
-    )
 }
 
 export default LeafletMap;
