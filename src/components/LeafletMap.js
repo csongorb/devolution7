8f651b7b465d375923d18d24bbeeb2c86cb1ca1b
Maria Shchurova <mary.shchurova@gmail.com>
2023-05-05 09:32:26 +0200

    warning fixed

--- src/components/LeafletMap.js
+++ src/components/LeafletMap.js
@@ -1,4 +1,4 @@
-import { MapContainer, useMapEvents, MapEvents, Marker, Popup, TileLayer } from 'react-leaflet';
+import { MapContainer, useMapEvents, Marker, Popup, TileLayer } from 'react-leaflet';
 import L from 'leaflet';
 
 
