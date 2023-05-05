e1c6bcf4ab4a62cdcf0e91ca5d9f28a216cd021e
Maria Shchurova <mary.shchurova@gmail.com>
2023-05-05 09:29:52 +0200

    Mouse Click lat/long logging (in console) and test popup in top right corner!

--- src/components/LeafletMap.js
+++ src/components/LeafletMap.js
@@ -1,41 +1,70 @@
-import React from 'react'
-import { MapContainer, TileLayer, Marker, Popup, Rectangle, ImageOverlay } from 'react-leaflet'
+import { MapContainer, useMapEvents, MapEvents, Marker, Popup, TileLayer } from 'react-leaflet';
+import L from 'leaflet';
 
 
-function LeafletMap ()
-{
-    const center = [51.505, -0.09]
+function LeafletMap() {
+  const mapExtent = [0.00000000, -39288.00000000, 33000.00000000, 0.00000000];
+  const mapMinZoom = 2;
+  const mapMaxZoom = 8;
+  const mapMaxResolution = 0.50000000;
+  const mapMinResolution = Math.pow(2, mapMaxZoom) * mapMaxResolution;
+  const tileExtent = [0.00000000, -39288.00000000, 33000.00000000, 0.00000000];
 
-    return(
-        <div >
-            <MapContainer  center={center} zoom={2} scrollWheelZoom={true} style={{height: 700, width : "100%"}}>
-              <TileLayer
-                noWrap={true}
-                name="Test"
-                url="https://raw.githubusercontent.com/csongorb/devolution7/master/src/components/CustomTiles/{z}/{x}/{y}.png"/>
+  const crs = L.CRS.Simple;
+  crs.transformation = new L.Transformation(1, -tileExtent[0], -1, tileExtent[3]);
+  crs.scale = function (zoom) {
+    return Math.pow(2, zoom) / mapMinResolution;
+  };
+  crs.zoom = function (scale) {
+    return Math.log(scale * mapMinResolution) / Math.LN2;
+  };
 
-              <Marker position={center}>
-                <Popup>
-                Link<br />
-                <a target="_blank" rel="noreferrer" href="https://devolution.online/">here</a>
-                </Popup>
-              </Marker>
+  const bounds = [
+    crs.unproject(L.point(mapExtent[2], mapExtent[3])),
+    crs.unproject(L.point(mapExtent[0], mapExtent[1]))
+  ]
 
-              <ImageOverlay
-              url="https://de.wikipedia.org/wiki/House#/media/Datei:Paradise_garage.jpg"
-              bounds={[center, [-35.8650, 154.2094]]}
-              />
+  const MapEvents = () => {
+    useMapEvents({
+      click(e) {
+        // setState your coords here
+        // coords exist in "e.latlng.lat" and "e.latlng.lng"
+        console.log(e.latlng.lat);
+        console.log(e.latlng.lng);
+      },
+    });
+    return false;
+}
 
-              <Rectangle
-                bounds={[center,[54, 6],]} pathOptions={{ color: 'cyan' }}>
-                <Popup>
-                <iframe width="300" height="169" src="https://www.youtube.com/embed/6O6V7L2IByg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-                </Popup>
-              </Rectangle>
+  return (
+    <div>
+      <MapContainer
+      center={[62.59334083012024, -90.52734375]} //hardcoded here, but bounds.GetCenter() results in empty viewport
+      zoom={2}  
+      minZoom={mapMinZoom}
+      maxZoom={mapMaxZoom}
+      bounds={bounds}
+      style={{height: 700, width : "100%"}} 
+      >
 
-            </MapContainer>
-        </div>
-    )
+      <TileLayer
+      minZoom={mapMinZoom}
+      maxZoom={mapMaxZoom}
+      
+      noWrap={true}
+      tms={false}
+      url="https://raw.githubusercontent.com/csongorb/devolution7/master/src/components/CustomTiles/{z}/{x}/{y}.png"/>
+            
+      <Marker position={[85.04686205001023, -179.94781494140628]}>
+        <Popup position={[85.04686205001023, -179.94781494140628]}>
+          Poput at [85.04686205001023, -179.94781494140628] !
+        </Popup>
+      </Marker>
+
+      <MapEvents />
+    </MapContainer>
+    </div>
+  );
 }
 
 export default LeafletMap;
