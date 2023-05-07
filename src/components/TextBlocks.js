b1fc92f5b114bd8fe1e0065715c9ebe5a0a9be9d
csongorb <csongorb@gmail.com>
2023-05-07 17:30:26 +0200

    Text optimizations.

--- src/components/TextBlocks.js
+++ src/components/TextBlocks.js
@@ -8,12 +8,58 @@ function TextBlocks() {
     <div className='text-block-container'>
     <div className="text-block">
       <h1>
-        Devolution #7
+      Devolution - Lounly Mountains: Downhill
       </h1>
-      Devolution is a game design focused project exploring the creative process and challenges of game development, consisting of exhibitions, tours, interviews, an archive, and more.
-      <br /> <br />
+      <br />
+      <a target="_blank" rel="noopener noreferrer" href="https://devolution.online">Devolution</a> is exploring and dissecting the creative process of game development, encountering new characteristics with every new edition.
       Each Devolution event / exhibition focuses on the evolution of a game through the exhibition of prototypes and interim versions and an in-depth conversation with the developers.
-      <br /> <br />
+      <br />
+      <br />
+      <table>
+        <tr>
+          <td width="49%">
+            Looking at games and game development from the perspective of Devolution lets you discover new and inspiring aspects of how games are growing into shape. In contrast to the usual reception of the results, Devolution reveals the whole road up to them.
+          </td>
+          <td width="2%">
+          </td>
+          <td width="49%">
+            Historically physical exhibitions of several in-between versions of a game (
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://devolution.online/category/events/1thecuriousexpedition/"
+                >The Curious Expedition</a>, <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://devolution.online/category/events/2shifthappens/"
+                >Shift Happens</a>, <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://devolution.online/category/events/3thumper/"
+                >Thumper</a>, <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://devolution.online/category/events/4superhot/"
+                >SUPERHOT</a>, <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://devolution.online/category/events/5summitexhibition/"
+                >Ape Out, Hidden Folks, Kingdom</a>, <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://devolution.online/6/"
+                >Baba Is You, Dicey Dungeons</a
+              >) and a summit (<a
+                href="https://devolution.online/gdasgardeningsummit/"
+                >Game Design as Gardening</a
+              >).
+          </td>
+        </tr>
+      </table>
+
+      <br />
+      Each Devolution event / exhibition focuses on the evolution of a game through the exhibition of prototypes and interim versions and an in-depth conversation with the developers.<br />
+      <br />
 
       <h3>
       How to grow a game?
@@ -30,10 +76,9 @@ function TextBlocks() {
     <h1>
       Total Digital
     </h1>
-
-      <p>
-      The online exhibition showcasing the development process of Lonely Mountains: Downhill is available for permanent viewing, offering visitors an opportunity to explore an interactive timeline of the game's evolution, downloadable and playable early prototypes and in-between versions of the game.
-      <br /> <br />
+      <br />
+      The online exhibition showcasing the development process of Lonely Mountains: Downhill is available for permanent viewing, offering visitors an opportunity to explore an interactive timeline of the game's evolution, downloadable and playable early prototypes and in-between versions of the game.<br />
+      <br />
       The exhibition works as a unique archive for…  <br />
       <ul>
         <li>Researchers to discover new aspects of how games are growing into shape</li>
@@ -41,7 +86,6 @@ function TextBlocks() {
         <li>Tutors and lecturers searching for inspiring use cases to teach with</li>
         <li>Just everybody interested in games to see and explore how they are developed</li>
       </ul>
-      </p>
 
     </div>
 
@@ -49,17 +93,25 @@ function TextBlocks() {
     <h1>
       Lonely Mountains Downhill
     </h1>
+    <br />
     Lonely Mountains: Downhill is a low-poly style mountain biking game developed by Berlin developer Megagon Industries. The game is a third-person biking game, where the player tries to connect with nature and complete courses on different mountains.
     </div>
 
     <div className="text-block">
-        <LeafletMap/>
+    <h1>
+      Timeline
+    </h1>
+      <br />
+      The timeline of the development process of Lonely Mountains: Downhill and other insights.<br />
+      <br />
+      <LeafletMap/>
     </div>
 
     <div className="text-block">
       <h1>
       Downloadable Versions
       </h1>
+      <br />
       <strong>Disclaimer: </strong>
       The versions of the game available here are historic development versions. We provide them for research purposes only and without warranty or support. They may not be fully functional, we cannot guarantee their compatibility with your device.
       The versions’ network features may be non-functional or limited. By downloading and using these versions, you agree that we do not take any responsibility for any issues that may occur as a result of such.
@@ -75,18 +127,20 @@ function TextBlocks() {
       <h1>
       A MAZE. / Berlin 2023
       </h1>
+      <br />
       The on-site exhibition focuses on the development process of Lonely Mountains: Downhill by displaying several in-between versions of the game, from early prototypes to later iterations. Visitors can explore the creative process behind the development, encountering new insights with each version they play and each comparison they make.
       <br />
       <table>
         <tr>
-        <td vertical-align="top" width= "50%">
+        <td width= "50%">
           <h2>
           Exhibition Tours
           </h2>
           For the ultimate insight into the development of Lonely Mountains: Downhill and its design decisions, join the guided tours through the exhibition held by the developers themselves, complementing the exhibited versions with background information, secrets and anecdotes.
         </td>
-
-        <td vertical-align="top">
+        <td width="2%">
+        </td>
+        <td width= "50%">
           <h3>
           Schedule
           </h3>
@@ -102,20 +156,26 @@ function TextBlocks() {
       </div>
 
       <div className="text-block">
-      <h1>
-      Credits
-      </h1>
+      <h1>Credits</h1>
+      <br />
 
       <table>
       <tr>
 
-      <td width= "50%" vertical-align="top">
-        Devolution #7 - Lonely Mountains: Downhill is a cooperation between <a target="_blank" rel="noopener noreferrer" href="https://devolution.online/">Devolution</a>
-        , A MAZE. / Berlin 2023, <a target="_blank" rel="noopener noreferrer" href="https://megagonindustries.com/"><br />Megagon Industries</a> and UE University of Europe for Applied Sciences.
+      <td width= "34%">
+      <h3>Partners</h3>
+      <br />
+      Devolution #7 - Lonely Mountains: Downhill is a cooperation between <a target="_blank" rel="noopener noreferrer" href="https://devolution.online/">Devolution</a>
+      , <a target="_blank" rel="noopener noreferrer" href="https://2023.amaze-berlin.de/">A MAZE. / Berlin 2023</a>, <a target="_blank" rel="noopener noreferrer" href="https://megagonindustries.com/">Megagon Industries</a>
+      and UE University of Europe for Applied Sciences.<br />
+      <br />
+      Huge THANK YOU to the developers for their trust, cooperation and support!
       </td>
-      <td>
+      <td width="2%">
+      </td>
+      <td width= "64%">
       <h3>Team</h3>
-
+      <br />
       Almat Kabdolov: (Concept Art, Graphic Design) <br />
       Anna Margrét Jónudóttir: (Exhibition Moderation, Timeline, Website) <br />
       Christoph Maureder: (Tech Advising) <br />
