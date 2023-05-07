8362ac6772b7357681f149cf863c7c319a7015a2
csongorb <csongorb@gmail.com>
2023-05-07 14:46:12 +0200

    Preventing build-error.

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,14 +2,12 @@ import React from 'react';
 import '../../App.css';
 import TextBlocks from '../TextBlocks';
 import Footer from '../Footer';
-import Cards from '../Cards'
 
 function Home()
 {
     return(
         <>
         <TextBlocks />
-        
         <Footer />
         </>
     );
